cc524a4eddfd08d28c95179c2a477dea2f7ecc5b
Sam <samsondav@protonmail.com>
2021-01-25 16:23:12 -0500

    Fix the migration in case a node op already added the index

--- core/store/migrations/migration1611388693/migrate.go
+++ core/store/migrations/migration1611388693/migrate.go
@@ -2,12 +2,11 @@ package migration1611388693
 
 import "github.com/jinzhu/gorm"
 
-// Migrate makes the explicit the pre-existing
-// implicit assumption that lowercase external initiator names are unique
+// Migrate adds the proper index to optimise the pipeline ORM for locking on runs instead of task runs
 func Migrate(tx *gorm.DB) error {
 	return tx.Exec(`
-		CREATE INDEX idx_pipeline_runs_unfinished_runs ON pipeline_runs (finished_at) WHERE finished_at IS NULL;
-		DROP INDEX idx_pipeline_task_runs_optimise_find_predecessor_unfinished_runs;
-		DROP INDEX idx_pipeline_task_runs_unfinished;
+		CREATE INDEX IF NOT EXISTS idx_pipeline_runs_unfinished_runs ON pipeline_runs (finished_at) WHERE finished_at IS NULL;
+		DROP INDEX IF EXISTS idx_pipeline_task_runs_optimise_find_predecessor_unfinished_runs;
+		DROP INDEX IF EXISTS idx_pipeline_task_runs_unfinished;
 	`).Error
 }
